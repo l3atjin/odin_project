479cbef005829d6ce1df777d1cab97ba5970b000
Batjin Lamjav <l3atjin@gmail.com>
2024-08-29 10:20:20 -0700

    finished pokemon memo game

--- react/memo-game/src/components/Board.jsx
+++ react/memo-game/src/components/Board.jsx
@@ -1,27 +1,61 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
+import PokeCard from './PokeCard';
+import '../App.css'
 
 export default function Board() {
   const [pokemon, setPokemon] = useState([]);
+  const [score, setScore] = useState(0);
+
+  const[memo, setMemo] = useState(new Set());
 
   useEffect(() => {
     async function fetchData(){
       try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+        const rand = Math.floor(Math.random() * 81);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${rand}&limit=20`);
         const data = await response.json();
-        console.log(data.results);
-        setPokemon(data);
+        const pokemonList = await Promise.all(data.results.map(async (poke, idx) => {
+          const {name, url} = poke;
+          const response = await fetch(url);
+          const pokeDetails = await response.json();
+          const imageUrl = pokeDetails.sprites.other["official-artwork"].front_default;
+          return {name, imageUrl};
+        }));
+        setPokemon(pokemonList);
       } catch (error) {
-        console.err(error);
+        console.error(error);
       }
     }
     fetchData();
     
 
-  }, []);
+  }, [score]);
+
+  const handleClick = (poke) => {
+    console.log("memo", memo);
+    console.log("pressed", poke.name);
+    // if it exists, reset
+    if (memo.has(poke.name)) {
+      setScore(0);
+      memo.clear();
+    } else {
+      memo.add(poke.name);
+      setScore(score + 1);
+    }
+    console.log("memo after", memo);
+  }
 
 
   return (
-    <div>board</div>
+    <>
+      <p>Score: {score}</p>
+      <div className='container'>
+        {pokemon.map((poke, idx) => {
+          return <PokeCard key={idx} poke={poke} onClick={() => handleClick(poke)}/>
+        })}
+      </div>
+    </>
+    
   )
 }
--- react/memo-game/src/components/PokeCard.jsx
+++ react/memo-game/src/components/PokeCard.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import '../App.css'
 
-export default function PokeCard() {
+export default function PokeCard({ poke, onClick }) {
+
   return (
-    <div>PokeCard</div>
+    <div onClick={onClick}>
+      <p className='text'>{poke.name}</p>
+      <img className="image" src={poke.imageUrl} alt="" />
+    </div>
   )
 }
